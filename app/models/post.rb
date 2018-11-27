95502b2f66829de97f50bd6ddf1a7b83249c0e4f
Ellen Wendelin <ellenwendelin@live.se>
2018-11-27 10:21:43 +0800

    validations for models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :restaurant
+
+  validates :name, presence: true
+  validates :original_price, numericality: true
+  validates :discount, numericality: { less_than: 1 }
+  validates :start_time, presence: true
+  validates :end_time, presence: true
 end
