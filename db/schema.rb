268d89129ec76167040ecf67041ccb6907bc9ad1
jiananwang <77843101@qq.com>
2018-11-29 18:05:57 +0800

    fix the search

--- db/schema.rb
+++ db/schema.rb
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2018_11_28_094420) do
   create_table "claims", force: :cascade do |t|
     t.bigint "post_id"
     t.bigint "user_id"
-    t.string "status"
+    t.string "status", default: "received"
     t.datetime "claimed_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 2018_11_28_094420) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "access_token"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
