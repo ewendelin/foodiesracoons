2ab45368e026de57be6a14bfb6232275d0054d56
Sz-Ting, Chen <kimikamis@gmail.com>
2018-11-30 12:06:32 +0800

    fixbug

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::RestaurantsController < Api::V1::BaseController
   def index
     post = Post.find(params[:post_id])
-    @restaurant = Restaurant.find_by(id: post.id)
+    @restaurant = Restaurant.find_by(id: post.restaurant_id)
     @rating = @restaurant.rating.map { |r| r.to_i }
     @posts = @restaurant.posts
   end
