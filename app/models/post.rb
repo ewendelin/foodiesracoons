c83e6a9656699b2ebd8b90bcf328b3c07d68d27e
Samir Bhojwani <sbhojwani1@gmail.com>
2018-11-30 12:31:15 +0800

    ADded image to Restaurant schema

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,13 +4,19 @@ class Post < ApplicationRecord
 
   validates :name, presence: true
   validates :original_price, numericality: true
-  validates :discount, numericality: { less_than: 1 }
+  validates :discount, numericality: { greater_than: 0, less_than_or_equal_to: 1 }
   validates :start_time, presence: true
   validates :end_time, presence: true
 
   def everyday_enum
     ['yes', 'no']
   end
+
+ #  def end_must_be_after_start
+ #   if :start_time >= :end_time
+ #     errors.add(:end_time, "must be after start time")
+ #   end
+ # end
 end
 
 
