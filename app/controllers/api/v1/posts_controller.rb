c99265fd4fa9aa7fe62b397aa852f930c4b28dc9
Samir Bhojwani <sbhojwani1@gmail.com>
2018-11-30 15:57:57 +0800

    A

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,8 +1,23 @@
 class Api::V1::PostsController < Api::V1::BaseController
   def index
-    @posts = Post.where("end_day > ?", Time.now)
+    @posts = Post.all
     if params[:keyword]
       @posts = Post.where("name = ?", params[:keyword])
+    end
+  end
+
+  def create
+    # byebug
+    # @claim = user.claims.new({
+    #   post_id: params[:post_id]
+    # })
+    # @claim.claimed_at = Time.now
+    @post = Post.new(params[:id])
+    # @restaurant = @post.restaurant
+    if @post.save
+      render 'index', status: :created
+    else
+      render_error
     end
   end
 
